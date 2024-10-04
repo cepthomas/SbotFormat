a19fc719501695f5136026b18c24e07423d0d0ed
cepthomas <cepthomas@gmail.com>
2024-10-04 15:52:02 -0500

    Cleaning up test structure.

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,50 +1,23 @@
 import sys
 import os
-import platform
-import pdb
 import unittest
 from unittest.mock import MagicMock
 
-import sublime
-import sublime_plugin
+# Add path to code under test.
+test_path = os.path.join(os.path.dirname(__file__), '..')
+if test_path not in sys.path:
+      sys.path.insert(0, test_path)
 
-from . import sbot_common as sc
-from SbotFormat import sbot_format
-from SbotHighlight import sbot_highlight
-from Notr import notr, table
+# Now import the sublime emulation.
+import emu_sublime
+import emu_sublime_plugin
+sys.modules["sublime"] = emu_sublime
+sys.modules["sublime_plugin"] = emu_sublime_plugin
 
+# Now import the code under test.
+import sbot_format
 
-# TODO1 relocate to other packages.
-
-
-# Experiments with testing py code in general and ST plugins specifically.
-
-# print(f'Running {__file__} with python {platform.python_version()} on {platform.platform()}')
-
-
-# Exclude unittests from production builds
-# ->
-# # git
-# .github/ export-ignore
-# .git export-ignore
-# .gitignore export-ignore
-# .gitattributes export-ignore
-# # development utilities
-# /dev/ export-ignore
-# .travis.yml export-ignore
-# tox.ini export-ignore
-# # unittests
-# tests/ export-ignore
-# # example files
-# /example-*.json export-ignore
-
-# Add root unittesting.json:
-# {
-#     "tests_dir": "package_control/tests",
-#     "pattern": "test*.py",
-#     "verbosity": 1
-# }
-
+import sbot_common as sc
 
 
 #-----------------------------------------------------------------------------------
